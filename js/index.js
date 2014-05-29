f86991dfca5e5d23dc612a31effd76eba368871c
aspin <kevinchen@u.northwestern.edu>
2014-05-29 04:33:48 +0100

    fixed up message box and maps functionality

--- js/index.js
+++ js/index.js
@@ -2,8 +2,13 @@
     "use strict";
 
     var data = [];
-    var prev_lat=0;
-    var prev_lon=0;
+
+    var messageHistory = [];
+    var map = 1234;
+
+    for(var i=0; i<10; i++) {
+        messageHistory.push("");
+    }
 
     // Outputs some logs about jwplayer
     function print(t,obj) {
@@ -15,10 +20,11 @@
 
     $(document).ready(function () {
         Parse.initialize("3dgBmw9ZzGVprNrdoNuQZ4TgmWzjkc8rc5HT3quP", "zFyHtXjR0PTbeqCNgSVMJgiMndBWVEi4Qu8F1I1y");
+        getLocation();
         setInterval(function() {getLocation();}, 5000);
         initMessage();
 
-         startPlayer($('#stream').val());
+        startPlayer($('#stream').val());
 
         $('#start').click(function () {
              startPlayer($('#stream').val());         
@@ -28,6 +34,11 @@
         });
         $('#submit').click(function() {
             changeMessage($('#message').val());
+        })
+        $('#message').keypress(function (e) {
+            if (e.which == 13) {
+                changeMessage($('#message').val());
+            }
         })
     });
 
@@ -57,7 +68,6 @@
         });
 
         jwplayer('player').play();
-
     }
 
     function initMessage() {
@@ -92,6 +102,7 @@
     }
 
     function changeMessage(word) {
+        messageHistory.shift();
         var word;
         var messageObject = Parse.Object.extend("message");
         var messageQuery = new Parse.Query(messageObject);
@@ -117,11 +128,20 @@
                         console.log("sad face");
                     }
                 })
+                messageHistory.push("<strong> You said: </strong> " +word);
+                updateMessageHistory();
             },
             error: function(error) {
                 console.log("found nothing");
             }
         });
+    }
+
+    function updateMessageHistory() {
+        $('#history').empty();
+        for(var i=0; i<10; i++) {
+            $('#history').append(messageHistory[i] + " <br/>")
+        }
     }
 
     function getLocation() {
@@ -140,47 +160,25 @@
                     lon = results[0].get("lon");
                 }
                 if (lat != "??") {
-                    renderMap(lat, lon);
+                    var loc = new google.maps.LatLng(lat, lon);
+                    if (map == 1234) {
+                        initMap(loc);
+                    }
+                    map.setCenter(loc);
                     console.log(lat);
                     console.log(lon);
                 }
             }
         })
-
     }
 
-    function renderMap(lat, lon) {
+    function initMap(loc) {
+        var mapOptions = {
+            zoom: 18,
+            center: loc
+          }
 
-        var dist = measure(lat, lon, prev_lat, prev_lon);
-
-        if (dist > 25) {
-            var loc = new google.maps.LatLng(lat, lon);
-            var mapOptions = {
-                zoom: 18,
-                center: loc
-              }
-
-            var map = new google.maps.Map(document.getElementById('location'), mapOptions);
-            prev_lat = lat;
-            prev_lon = lon;
-
-        }
-        else {
-            console.log("no update made.");
-        }
-
-    }
-
-    function measure(lat1, lon1, lat2, lon2){  // generally used geo measurement function
-        var R = 6378.137; // Radius of earth in KM
-        var dLat = (lat2 - lat1) * Math.PI / 180;
-        var dLon = (lon2 - lon1) * Math.PI / 180;
-        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-        Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
-        Math.sin(dLon/2) * Math.sin(dLon/2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        var d = R * c;
-        return d * 1000; // meters
+        map = new google.maps.Map(document.getElementById('location'), mapOptions);
     }
 
 
