354592a5175480d76316791a22e5fbcfca82d496
aspin <tsunami70875@gmail.com>
2014-10-24 03:46:37 -0500

    fixxed https issue

--- client/views/streams/paper_stream.js
+++ client/views/streams/paper_stream.js
@@ -1,5 +1,4 @@
 Template.paperStream.rendered = function () {
-    setInterval(function() { getTap(); }, 1000); // figure out a better way?
 
     var apiKey = "45040222";
     var sessionId = "1_MX40NTA0MDIyMn5-MTQxNDA5NTYxNjEyMH5maGZKSVREODc0MkM0K0MvR3kvLzgrQ2l-fg";
@@ -18,26 +17,3 @@ Template.paperStream.rendered = function () {
     // need some handlers with play / pause buttons
 
 };
-
-function getTap() {
-    var tapObject = Parse.Object.extend("taps")
-    var tapQuery = new Parse.Query(tapObject)
-
-    // tapQuery.first({
-    //     success: function(obj) {
-    //         if (obj.get("new")) {
-    //             showTap(obj.get("x"), obj.get("y"));
-    //             obj.set("new", false);
-    //             obj.save();
-    //         }
-    //     },
-    //     error: function(error) {
-    //         console.log("Error!" + error)
-    //     }
-    // })
-}
-
-function showTap(x, y) {
-    console.log("X: " + x)
-    console.log("Y: " + y)
-}
