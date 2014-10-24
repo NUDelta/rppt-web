cb4c466112aa914e19f70f286e5945cab5b5f526
aspin <tsunami70875@gmail.com>
2014-10-24 00:57:48 -0500

    ported maps function

---
+++ client/views/cc.js
@@ -0,0 +1,4 @@
+Template.cc.rendered = function () {
+    Parse.initialize("3dgBmw9ZzGVprNrdoNuQZ4TgmWzjkc8rc5HT3quP", "zFyHtXjR0PTbeqCNgSVMJgiMndBWVEi4Qu8F1I1y");
+    map = 1234;
+};
---
+++ client/views/streams/map.js
@@ -0,0 +1,40 @@
+Template.map.rendered = function () {
+    setInterval(function() { getLocation(); }, 5000); // figure out a better way?
+
+    function getLocation() {
+        var lat = "??";
+        var lon = "??";
+        var locationObject = Parse.Object.extend("location");
+        var locationQuery = new Parse.Query(locationObject);
+
+        locationQuery.find( {
+            success: function(results) {
+                if (results.length == 0) {
+                   // make new objects
+                }
+                else {
+                    lat = results[0].get("lat");
+                    lon = results[0].get("lon");
+                }
+                if (lat != "??") {
+                    var loc = new google.maps.LatLng(lat, lon);
+                    if (map == 1234) {
+                        initMap(loc);
+                    }
+                    map.setCenter(loc);
+                    console.log(lat);
+                    console.log(lon);
+                }
+            }
+        })
+    }
+
+    function initMap(loc) {
+        var mapOptions = {
+            zoom: 18,
+            center: loc
+          }
+
+        map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    }
+};
