068ad1950c1f7ecfe1deef98d5d2fecf113a919f
aspin <tsunami70875@gmail.com>
2014-11-09 17:11:40 -0600

    added panning tracker

--- client/views/cc.js
+++ client/views/cc.js
@@ -2,6 +2,7 @@ Template.cc.rendered = function () {
     
     Parse.initialize("3dgBmw9ZzGVprNrdoNuQZ4TgmWzjkc8rc5HT3quP", "zFyHtXjR0PTbeqCNgSVMJgiMndBWVEi4Qu8F1I1y");
     map = null;
+    panning = false;
 
     Meteor.call("clearAllGestures");
     Gestures.find().observeChanges({
@@ -9,20 +10,40 @@ Template.cc.rendered = function () {
             if (fields["action"] == "tap") {
                 showTap(fields["params"]);
             }
+            else if (fields["action"] == "pan") {
+                showPan(fields["params"]);
+            }
         }
     });
 };
 
 function showTap(params) {
-    var x = params["x"];
-    var y = params["y"];
-
     var offset = 15; // from CSS
-    $("#tap-indicator").css("left", x-15);
-    $("#tap-indicator").css("top", -460+y-15);
+    
+    x = params["x"] - offset;
+    y = params["y"] - offset - 460;
+
+    $("#tap-indicator").css("left", x);
+    $("#tap-indicator").css("top", y);
     $("#tap-indicator").show()
 
-    setTimeout(function() { resetTap(); }, 800);
+    // setTimeout(function() { resetTap(); }, 800);
+}
+
+function showPan(params) {
+    panning = true;
+
+    xshifted = x + params["x"];
+    yshifted = y + params["y"];
+
+    $("#tap-indicator").css("left", xshifted);
+    $("#tap-indicator").css("top", yshifted);
+
+    panning = false;
+    // setTimeout(function() {
+    //     if (!panning)
+    //         resetTap();
+    // }, 800);
 }
 
 function resetTap() {
--- server/methods.js
+++ server/methods.js
@@ -1,7 +1,12 @@
 Meteor.methods({
-  createTap: function (tapObject) {
+  createTap: function(tapObject) {
     var tapAction = {"done": false, "action": "tap", "params": tapObject};
     Gestures.insert(tapAction);
+  },
+
+  panUpdate: function(panObject) {
+    var panUpdate = {"done": false, "action": "pan", "params": panObject};
+    Gestures.insert(panUpdate);
   },
 
   clearAllGestures: function() {
