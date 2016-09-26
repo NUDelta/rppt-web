e7c1267d68eb56ecaf757d8eed76500c8de16dcd
Alex Kaldjian <alex.kaldjian@gmail.com>
2016-09-26 15:56:00 -0500

    created new branch

--- server/lib/environment.js
+++ server/lib/environment.js
@@ -1,7 +1,7 @@
 // const key = 'YOUR_API_KEY' // e.g. '45357722'
 // const secret = 'YOUR_SECRET_KEY' // e.g. 'b69bc69eeb464847b4d85bab5fd9a69406a988e1'
 
-key = '45423292';
+key = '45692792' //'45423292';
 // const secret = Assets.getText('secret');
-const secret = '829278fab57b857e550a944f8b240476066da49b';
+const secret = 'eeca00124e924f38128aa7ded39bbda10c73eb1f'//'829278fab57b857e550a944f8b240476066da49b';
 openTokClient = new OpenTokClient(key, secret);
